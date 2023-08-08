006fe774cb1b56ff76f5b630964db29798e3b47f
Thomas Lemaire <contact@thomaslemaire.dev>
2023-08-08 16:33:57 +0200

    Migrations clean-up

--- migrations/Version20230804131413.php
+++ migrations/Version20230804131413.php
@@ -7,26 +7,20 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230804131413 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Table geonames_country';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE geonames_country (id INT NOT NULL, continent VARCHAR(10) DEFAULT NULL, country_code VARCHAR(10) DEFAULT NULL, capital VARCHAR(255) DEFAULT NULL, languages VARCHAR(255) DEFAULT NULL, geoname_id INT NOT NULL, south DOUBLE PRECISION NOT NULL, north DOUBLE PRECISION NOT NULL, east DOUBLE PRECISION NOT NULL, west DOUBLE PRECISION NOT NULL, iso_alpha3 VARCHAR(10) DEFAULT NULL, fips_code VARCHAR(10) DEFAULT NULL, population INT NOT NULL, iso_numeric INT DEFAULT NULL, area_in_sq_km VARCHAR(10) DEFAULT NULL, country_name VARCHAR(255) DEFAULT NULL, continent_name VARCHAR(255) DEFAULT NULL, currency_code VARCHAR(10) DEFAULT NULL, lat NUMERIC(10, 6) DEFAULT NULL, lng NUMERIC(10, 6) DEFAULT NULL, geojson VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE geonames_country');
     }
 }
--- migrations/Version20230807092330.php
+++ migrations/Version20230807092330.php
@@ -7,30 +7,20 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230807092330 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Table geonames_administrative_division';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE geonames_administrative_division ADD country_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE geonames_administrative_division ADD fcl VARCHAR(10) DEFAULT NULL');
-        $this->addSql('ALTER TABLE geonames_administrative_division ADD srtm3 INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE geonames_administrative_division (id INT NOT NULL, geoname_id INT NOT NULL, name VARCHAR(255) NOT NULL, ascii_name VARCHAR(255) DEFAULT NULL, toponym_name VARCHAR(255) DEFAULT NULL, continent_code VARCHAR(255) DEFAULT NULL, cc2 VARCHAR(255) DEFAULT NULL, country_code VARCHAR(255) DEFAULT NULL, country_id INT DEFAULT NULL, admin_name1 VARCHAR(255) DEFAULT NULL, admin_name2 VARCHAR(255) DEFAULT NULL, admin_name3 VARCHAR(255) DEFAULT NULL, admin_name4 VARCHAR(255) DEFAULT NULL, admin_name5 VARCHAR(255) DEFAULT NULL, admin_id1 INT DEFAULT NULL, admin_id2 INT DEFAULT NULL, admin_id3 INT DEFAULT NULL, admin_id4 INT DEFAULT NULL, admin_id5 INT DEFAULT NULL, admin_code1 VARCHAR(255) DEFAULT NULL, admin_code2 VARCHAR(255) DEFAULT NULL, admin_code3 VARCHAR(255) DEFAULT NULL, admin_code4 VARCHAR(255) DEFAULT NULL, lat NUMERIC(10, 6) DEFAULT NULL, lng NUMERIC(10, 6) DEFAULT NULL, population INT DEFAULT NULL, timezone_gmt_offset INT DEFAULT NULL, timezone_time_zone_id VARCHAR(255) DEFAULT NULL, timezone_dst_offset INT DEFAULT NULL, admin_type_name VARCHAR(255) DEFAULT NULL, fcode VARCHAR(255) DEFAULT NULL, geojson VARCHAR(255) DEFAULT NULL, fcl VARCHAR(10) DEFAULT NULL, srtm3 INT DEFAULT NULL, astergdem INT DEFAULT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE geonames_administrative_division DROP country_id');
-        $this->addSql('ALTER TABLE geonames_administrative_division DROP fcl');
-        $this->addSql('ALTER TABLE geonames_administrative_division DROP srtm3');
+        $this->addSql('DROP TABLE geonames_administrative_division');
     }
 }
