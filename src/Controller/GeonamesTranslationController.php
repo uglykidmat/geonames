913645ba26d0063ff1006b02a094fcb09b34cde1
MathieuGTR <mathieu@gtrsuite.com>
2023-08-17 11:34:27 +0200

    Controller update with 2 POST errors + install api package

--- src/Controller/GeonamesTranslationController.php
+++ src/Controller/GeonamesTranslationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\GeonamesTranslation;
 use Doctrine\ORM\EntityManagerInterface;
+use JsonException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
