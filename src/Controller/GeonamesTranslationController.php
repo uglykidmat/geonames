c8ccb3d88f5ad6d25f3f994297a3e5c867482584
MathieuGTR <mathieu@gtrsuite.com>
2023-08-17 13:56:29 +0200

    POST method finalization

--- src/Controller/GeonamesTranslationController.php
+++ src/Controller/GeonamesTranslationController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\GeonamesTranslation;
 use Doctrine\ORM\EntityManagerInterface;
-use JsonException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,29 +24,92 @@ class GeonamesTranslationController extends AbstractController
     }
 
     #[Route('/', name: 'translation_post', methods: ['POST'])]
-    public function post(Request $postRequest): JsonResponse
+    public function post(Request $postRequest, EntityManagerInterface $translationEntityManager): JsonResponse
     {
         $postResponse = new JsonResponse();
 
+        //________________________________Media type check
         if ($postRequest->getContentTypeFormat() != 'json' || !$postRequest->getContent()) {
             $postResponse->setStatusCode(415);
-            $postResponse->setContent('{"Error" : "Unsupported Media Type"}');
+            $postResponse->setContent('{"Error" : "Unsupported Media Type, expected content-type application/JSON"}');
 
             return $postResponse;
         }
 
-        @json_decode($postRequest->getContent());
-        $postRequesterr = json_last_error_msg();
-        //dd($postRequesterr);
+        //________________________________JSON Syntax check
+        $postContent = (array) @json_decode($postRequest->getContent());
+
         if (!(json_last_error() === JSON_ERROR_NONE)){
             $postResponse->setStatusCode(422);
-            $postResponse->setContent('{"Json error" : "' . $postRequesterr . '"}');
+            $postResponse->setContent('{"Json error" : "' . json_last_error_msg() . '"}');
 
             return $postResponse;
         }
 
+        //________________________________Translation fields check
+        $errorsInPostContent = false;
+
+        foreach ($postContent as $postKey => $postValue) {
+            $postValue = (array)$postValue;
+            if(
+                !isset($postValue["geonameId"]) ||
+                !isset($postValue["name"]) ||
+                !isset($postValue["countryCode"]) ||
+                !isset($postValue["fcode"]) ||
+                !isset($postValue["locale"]) ||
+                ($postValue["geonameId"] == null) ||
+                ($postValue["name"] == null) ||
+                ($postValue["countryCode"] == null) ||
+                ($postValue["fcode"] == null) ||
+                ($postValue["locale"] == null) ||
+                $errorsInPostContent == true
+            )
+            {
+                $errorsInPostContent = true;
+                $postResponse->setStatusCode(400);
+                $postResponse->setContent('{"Json error" : "Missing fields or null values are not allowed"}');
+
+                return $postResponse;
+            }
+        }
+
+        $dbInsertionFound = array();
+        $dbInsertionDone = array();
+
+        //________________________________GeonameId check in repository
+        foreach ($postContent as $postKey => $postValue) {
+            $postValue = (array)$postValue;
+            if ($translationEntityManager->getRepository(GeonamesTranslation::class)
+            ->findByGeonameId($postValue["geonameId"])){
+                $dbInsertionFound[] = $postValue["geonameId"];
+            }
+            else{
+                $postTranslation = new GeonamesTranslation();
+                $postTranslation
+                ->setGeonameId($postValue["geonameId"])
+                ->setName($postValue["name"])
+                ->setCountryCode($postValue["countryCode"])
+                ->setFcode($postValue["fcode"])
+                ->setLocale($postValue["locale"]);
+
+                $translationEntityManager->persist($postTranslation);
+
+                $dbInsertionDone[] = $postValue["geonameId"];
+            }
+        }
+        $translationEntityManager->flush();
+
         $postResponse->setStatusCode(200);
-        $postResponse->setJson("YAY !");
+
+        if (count($dbInsertionDone) == 0){
+            $postResponse->setContent('{"POST" : "Success", "GeonameIds already found" : "'. implode(',',$dbInsertionFound) .'"}');
+        }
+        else if (count($dbInsertionFound) == 0){
+            $postResponse->setContent('{"POST" : "Success", "GeonameIds inserted" : "'. implode(',',$dbInsertionDone) .'"}');
+        }
+        else {
+            $postResponse->setContent('{"POST" : "Success", "GeonameIds already found" : "'. implode(',',$dbInsertionFound) .'", "GeonameIds inserted" : "'. implode(',',$dbInsertionDone) .'"}');
+        }
         
         return $postResponse;
     }
