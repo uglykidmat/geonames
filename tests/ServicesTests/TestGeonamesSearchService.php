4d6eb0d2640f61a8542c6391a2935a7dbdb6ffd1
MathieuGTR <mathieu@gtrsuite.com>
2023-08-30 14:25:41 +0200

    PHPUnit configuration file update, to pass warnings

---
+++ tests/ServicesTests/TestGeonamesSearchService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\tests\ServicesTests;
+
+use PHPUnit\Framework\TestCase;
+
+class TestGeonamesSearchService extends TestCase
+{
+    public function testBulkRequest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
