ba9bdac8dbe917df45466a5a5c22a9d64ae7c16a
MathieuGTR <mathieu@gtrsuite.com>
2023-08-30 14:24:49 +0200

    Unrelated alias added to the latlngsearch command

--- src/Command/LatLngSearchCommand.php
+++ src/Command/LatLngSearchCommand.php
@@ -7,13 +7,13 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'Latlngsearch',
     description: 'Launches a Geonames search with Latitude and Longitude data. This will return information about a GeonameId found close to the coordinates provided.',
+    aliases: ['lls']
 )]
 class LatLngSearchCommand extends Command
 {
