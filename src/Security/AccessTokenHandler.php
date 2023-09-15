979d2e8455f90274a42045a25a44d13a1dd8e059
MathieuGTR <mathieu@gtrsuite.com>
2023-09-15 15:30:40 +0200

    Configuration Security and Config yaml files + AccessTokenHandler

---
+++ src/Security/AccessTokenHandler.php
@@ -0,0 +1,25 @@
+<?php
+// src/Security/AccessTokenHandler.php
+namespace App\Security;
+
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
+
+class AccessTokenHandler implements AccessTokenHandlerInterface
+{
+    public function __construct(
+        private readonly string $token,
+        private readonly string $user,
+    ) {
+    }
+
+    public function getUserBadgeFrom(string $accessToken): UserBadge
+    {
+        if ($accessToken !== $this->token || null === $accessToken) {
+
+            throw new BadCredentialsException('Invalid credentials.');
+        }
+        return new UserBadge($this->user);
+    }
+}
