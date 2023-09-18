ce25714a2d3a560f746d2b19c3ea27fbfc4bd3b8
MathieuGTR <mathieu@gtrsuite.com>
2023-09-18 11:02:13 +0200

    use App\Entity\GeonamesCountry otherwise it tries to fetch a Service

--- src/Service/GeonamesSearchService.php
+++ src/Service/GeonamesSearchService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use stdClass;
+use App\Entity\GeonamesCountry;
 use App\Service\GeonamesAPIService;
 use App\Entity\GeonamesCountryLevel;
 use Psr\Cache\CacheItemPoolInterface;
