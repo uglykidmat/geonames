3d7f392433cb0d96a6c9d90f960423a386726600
Remi <remi@gtrsuite.com>
2023-09-18 14:40:34 +0200

    feat(GeoServiceBulk): refacto

--- src/Service/GeonamesSearchService.php
+++ src/Service/GeonamesSearchService.php
@@ -2,6 +2,7 @@
 // src/Service/GeonamesSearchService.php
 namespace App\Service;
 
+use App\Entity\GeonamesAdministrativeDivision;
 use stdClass;
 use App\Entity\GeonamesCountry;
 use App\Service\GeonamesAPIService;
@@ -22,110 +23,68 @@ class GeonamesSearchService
         private AdminCodesMapperService $adminCodesMapperService,
         private string $redisDsn
     ) {
-        $this->redisCache = $redisCache;
+        $this->gclRepository = $this->entityManager->getRepository(GeonamesCountryLevel::class);
     }
 
     public function bulkRequest(?string $request): string
     {
-        $bulkResponse = json_decode($request);
-        $gclRepository = $this->entityManager->getRepository(GeonamesCountryLevel::class);
+        $bulkResponse = [];
+        $bulkRequest = json_decode($request);
 
-        foreach ($bulkResponse as $bulkIndex => $bulkRow) {
+        foreach ($bulkRequest as $bulkIndex => $bulkRow) {
 
-            if (self::checkRequestContents($bulkRow) == "coordinates") {
+            // defaults output values
+            $bulkResponse[$bulkIndex] = [
+                ...(array)$bulkRow,
+                'error' => false,
+                'used_level' => 1,
+            ];
 
-                $cacheKey = 'geonames_latlng_' .
-                    $bulkRow->lat .
-                    '-' .
-                    $bulkRow->lng;
-
-                $cachedData = $this->redisCache->getItem($cacheKey);
-
-                if (!$cachedData->isHit()) {
-                    $geoIdFound = $this->apiService->latLngSearch(
-                        $bulkRow->lat,
-                        $bulkRow->lng
-                    );
-                    if (!$IdFoundInDb = $this->dbCachingService->searchSubdivisionInDatabase($geoIdFound)) {
-                        $idToSave = $this->apiService->getJsonSearch($geoIdFound);
-
-                        $this->dbCachingService->saveSubdivisionToDatabase($idToSave);
-                    }
-
-                    $UsedLevel = ($gclRepository->findOneByCountryCode(
-                        $IdFoundInDb->getCountryCode()
-                    ) ?? (new GeonamesCountryLevel()))->getUsedLevel();
-
-                    $adminCodesArray = $this->adminCodesMapperService->codesMapper($IdFoundInDb, $UsedLevel);
-
-                    $bulkResponse[$bulkIndex] = [
-                        ...(array)$bulkResponse[$bulkIndex],
-                        ...['error' => false],
-                        ...['used_level' => $UsedLevel],
-                        ...['country_code' => $IdFoundInDb->getCountryCode()],
-                        ...$adminCodesArray
-                    ];
-
-                    $cachedData->set($bulkResponse[$bulkIndex]);
-                    $this->redisCache->save($cachedData);
-                } else {
-                    $bulkResponse[$bulkIndex] = $cachedData->get();
-                }
-            } else if (self::checkRequestContents($bulkRow) == "zipcode") {
-
-                $cacheKey = 'geonames_country-zipcode_' .
-                    $bulkRow->country_code .
-                    "-" .
-                    $bulkRow->zip_code;
-
-                $cachedData = $this->redisCache->getItem($cacheKey);
-
-                if (!$cachedData->isHit()) {
-                    $geonamesZipCodeFound = $this->apiService->postalCodeLookupJSON(
-                        $bulkRow->zip_code,
-                        $bulkRow->country_code
-                    );
-
-                    $geoIdFound = $this->apiService->latLngSearch(
-                        $geonamesZipCodeFound['postalcodes'][0]['lat'],
-                        $geonamesZipCodeFound['postalcodes'][0]['lng']
-                    );
-
-                    if (!$IdFoundInDb = $this->dbCachingService->searchSubdivisionInDatabase($geoIdFound)) {
-                        $idToSave = $this->apiService->getJsonSearch($geoIdFound);
-                        $this->dbCachingService->saveSubdivisionToDatabase($idToSave);
-                    }
-                    $IdFoundInDb = $this->dbCachingService->searchSubdivisionInDatabase($geoIdFound);
-
-                    $UsedLevel = ($gclRepository->findOneByCountryCode(
-                        $IdFoundInDb->getCountryCode()
-                    ) ?? (new GeonamesCountryLevel()))->getUsedLevel();
-
-                    $adminCodesArray = $this->adminCodesMapperService->codesMapper($IdFoundInDb, $UsedLevel);
-
-                    $bulkResponse[$bulkIndex] = [
-                        ...(array)$bulkResponse[$bulkIndex],
-                        ...['error' => false],
-                        ...['lat' => $IdFoundInDb->getLat(), 'lng' => $IdFoundInDb->getLng()],
-                        ...['used_level' => $UsedLevel],
-                        ...['country_code' => $IdFoundInDb->getCountryCode()],
-                        ...$adminCodesArray
-                    ];
-
-                    $cachedData->set($bulkResponse[$bulkIndex]);
-                    $this->redisCache->save($cachedData);
-                } else {
-                    $bulkResponse[$bulkIndex] = $cachedData->get();
-                }
-            } else {
-                $bulkResponse[$bulkIndex] = [
-                    ...(array)$bulkResponse[$bulkIndex],
-                    ...['error' => true, 'message' => 'Not found by lat-lng coordinates, nor Country-ZipCode']
-                ];
+            $requestType = self::checkRequestContents($bulkRow);
+            if (!$requestType) {
+                // set error for this row
+                $bulkResponse[$bulkIndex]['error'] = true;
+                $bulkResponse[$bulkIndex]['message'] = 'Not found by lat-lng coordinates, nor Country-ZipCode';
+                // return error & jump next row
+                continue;
             }
+
+            // first get redis cache if exist
+            $cacheKey = $this->getCacheKey($requestType, $bulkRow);
+            if (($geodata = $this->hasCache($cacheKey))) {
+                // set response for this row
+                $bulkResponse[$bulkIndex] = [
+                    'elt_id' => $bulkRow->elt_id ?? $bulkIndex,
+                    ...$geodata,
+                ];
+                // return cache & jump next row
+                continue;
+            }
+
+            // get
+            switch ($requestType) {
+                case 'coordinates':
+                    $geodata = $this->getByCoodinates($bulkRow);
+                    break;
+                case 'zipcode':
+                    $geodata = $this->getByZipcode($bulkRow);
+                    break;
+                default:
+                    // this case should never append due to previous $requestType error check
+                    $geodata['error'] = true;
+            }
+
+            // set for next iteration
+            $this->setCache($cacheKey, $geodata);
+
+            // set response for this row
+            $bulkResponse[$bulkIndex] = [
+                'elt_id' => $bulkRow->elt_id ?? $bulkIndex,
+                ...$geodata,
+            ];
         }
 
-        return json_encode($bulkResponse);
+        return json_encode($bulkResponse, JSON_THROW_ON_ERROR);
     }
 
     private function checkRequestContents(stdClass $requestEntry): string
@@ -149,6 +108,111 @@ class GeonamesSearchService
             return "zipcode";
         }
 
-        return "Missing or incorrect fields";
+        return false;
+    }
+
+    public function getByCoodinates(stdClass $bulkRow): array
+    {
+        $geoIdFound = $this->apiService->latLngSearch(
+            $bulkRow->lat,
+            $bulkRow->lng
+        );
+
+        $geoDivision = $this->getGeoDivision($geoIdFound);
+        $UsedLevel = $this->getLevel($geoDivision);
+        $adminCodesArray = $this->adminCodesMapperService->codesMapper($geoDivision, $UsedLevel);
+
+        return [
+            'used_level' => $UsedLevel,
+            'country_code' => $geoDivision->getCountryCode(),
+            ...$adminCodesArray
+        ];
+    }
+
+    public function getByZipcode(mixed $bulkRow): array
+    {
+        $geonamesZipCodeFound = $this->apiService->postalCodeLookupJSON(
+            $bulkRow->zip_code,
+            $bulkRow->country_code
+        );
+
+        $geoIdFound = $this->apiService->latLngSearch(
+            $geonamesZipCodeFound['postalcodes'][0]['lat'],
+            $geonamesZipCodeFound['postalcodes'][0]['lng']
+        );
+
+        $geoDivision = $this->getGeoDivision($geoIdFound);
+        $UsedLevel = $this->getLevel($geoDivision);
+        $adminCodesArray = $this->adminCodesMapperService->codesMapper($geoDivision, $UsedLevel);
+
+        return [
+            'lat' => $geoDivision->getLat(),
+            'lng' => $geoDivision->getLng(),
+            'used_level' => $UsedLevel,
+            'country_code' => $geoDivision->getCountryCode(),
+            ...$adminCodesArray
+        ];
+    }
+
+    // TODO : externalize those method in CacheRedisService
+    public function getCacheKey(string $requestType, stdClass $bulkRow): string
+    {
+        switch ($requestType) {
+            case 'coordinates':
+                $cacheKey = 'geonames_latlng_' . $bulkRow->lat . '-' . $bulkRow->lng;
+                break;
+            case 'zipcode':
+                $cacheKey = 'geonames_country-zipcode_' . $bulkRow->country_code . "-" . $bulkRow->zip_code;
+                break;
+            default:
+                $v = false;
+        }
+        return $cacheKey;
+    }
+
+    public function hasCache(string $cacheKey): bool|array
+    {
+        if (empty($cacheKey)) {
+            return false;
+        }
+        $cachedData = $this->redisCache->getItem($cacheKey);
+        if ($cachedData->isHit()) {
+            return $cachedData->get();
+        }
+
+        return false;
+    }
+
+    public function setCache(string $cacheKey, array $geoData): bool
+    {
+        if (empty($cacheKey)) {
+            return false;
+        }
+
+        $cachedData = $this->redisCache->getItem($cacheKey);
+
+        $cachedData->set($geoData);
+        $this->redisCache->save($cachedData);
+
+        return true;
+    }
+
+    public function getGeoDivision(?int $geoIdFound): ?GeonamesAdministrativeDivision
+    {
+        if ($IdFoundInDb = $this->dbCachingService->searchSubdivisionInDatabase($geoIdFound)) {
+            return $IdFoundInDb;
+        }
+
+        $idToSave = $this->apiService->getJsonSearch($geoIdFound);
+        $this->dbCachingService->saveSubdivisionToDatabase($idToSave);
+
+        return $this->dbCachingService->searchSubdivisionInDatabase($geoIdFound);
+    }
+
+    public function getLevel(GeonamesAdministrativeDivision|null $IdFoundInDb): ?int
+    {
+        return ($this->gclRepository->findOneByCountryCode(
+            $IdFoundInDb->getCountryCode()
+        ) ?? (new GeonamesCountryLevel()))->getUsedLevel();
     }
 }
