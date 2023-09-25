f4933cde91cf5673ea3fb19ced09661246e7978c
MathieuGTR <mathieu@gtrsuite.com>
2023-09-25 15:45:14 +0200

    Repository updates

--- src/Repository/GeonamesCountryLevelRepository.php
+++ src/Repository/GeonamesCountryLevelRepository.php
@@ -21,28 +21,37 @@ class GeonamesCountryLevelRepository extends ServiceEntityRepository
         parent::__construct($registry, GeonamesCountryLevel::class);
     }
 
-//    /**
-//     * @return GeonamesCountryLevel[] Returns an array of GeonamesCountryLevel objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('g.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return GeonamesCountryLevel[] Returns an array of GeonamesCountryLevel objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('g')
+    //            ->andWhere('g.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('g.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?GeonamesCountryLevel
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?GeonamesCountryLevel
+    //    {
+    //        return $this->createQueryBuilder('g')
+    //            ->andWhere('g.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+    public function findUsedLevelMoreThan($value)
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.usedLevel >= :val')
+            ->andWhere('g.usedLevel != 0')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getResult();
+    }
 }
--- src/Repository/GeonamesCountryLocaleRepository.php
+++ src/Repository/GeonamesCountryLocaleRepository.php
@@ -21,28 +21,28 @@ class GeonamesCountryLocaleRepository extends ServiceEntityRepository
         parent::__construct($registry, GeonamesCountryLocale::class);
     }
 
-//    /**
-//     * @return GeonamesCountryLocale[] Returns an array of GeonamesCountryLocale objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('g.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return GeonamesCountryLocale[] Returns an array of GeonamesCountryLocale objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('g')
+    //            ->andWhere('g.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('g.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?GeonamesCountryLocale
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?GeonamesCountryLocale
+    //    {
+    //        return $this->createQueryBuilder('g')
+    //            ->andWhere('g.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
