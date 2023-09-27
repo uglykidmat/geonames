d7285041cfaf84f507a51cd1c1bd9f884b345ed7
MathieuGTR <mathieu@gtrsuite.com>
2023-09-27 13:11:32 +0200

    Basic UPDATE queries

---
+++ migrations/Version20230927110512.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230927110512 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('UPDATE geonames_country_level SET max_level = 1 WHERE max_level = 0');
+        $this->addSql('UPDATE geonames_country_level SET used_level = 1 WHERE used_level = 0');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA IF NOT EXISTS public');
+    }
+}
