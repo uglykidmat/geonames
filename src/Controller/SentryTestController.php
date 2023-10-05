77c0a61a404a3c34f7e47ac104c974627f485bd2
MathieuGTR <mathieu@gtrsuite.com>
2023-10-05 15:09:12 +0200

    feat(sentry): first test Controller

--- src/Controller/SentryTestController.php
+++ src/Controller/SentryTestController.php
@@ -2,31 +2,26 @@
 
 namespace App\Controller;
 
-use RuntimeException;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class SentryTestController extends AbstractController
 {
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(private LoggerInterface $logger)
     {
         $this->logger = $logger;
     }
 
-    /**
-     * @Route(name="sentry_test", path="/_sentry-test")
-     */
     #[Route(name: 'sentry_test', path: '/_sentry-test')]
     public function testLog()
     {
+
         // the following code will test if monolog integration logs to sentry
-        $this->logger->error('My custom monolog error.');
+        $this->logger->error('My custom error.');
 
         // the following code will test if an uncaught exception logs to sentry
         throw new RuntimeException('Sentry test error.');
