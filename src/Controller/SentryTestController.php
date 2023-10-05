e26f1403845505ecc1b1bdb5624e8d27662c35f1
MathieuGTR <mathieu@gtrsuite.com>
2023-10-05 16:59:09 +0200

    chore(sentry): update config following ThomasL's advice

--- src/Controller/SentryTestController.php
+++ src/Controller/SentryTestController.php
@@ -7,21 +7,18 @@ use RuntimeException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
 class SentryTestController extends AbstractController
 {
-
     public function __construct(private LoggerInterface $logger)
     {
         $this->logger = $logger;
     }
 
-    #[Route(name: 'sentry_test', path: '/_sentry-test')]
+    #[Route(name: 'sentry_test', path: '/_sentry-test', env: 'dev')]
     public function testLog()
     {
-
         // the following code will test if monolog integration logs to sentry
-        $this->logger->error('My custom error.');
+        $this->logger->error('Monolog error.', ['Cause' => 'this is a test']);
 
         // the following code will test if an uncaught exception logs to sentry
         throw new RuntimeException('Sentry test error.');
