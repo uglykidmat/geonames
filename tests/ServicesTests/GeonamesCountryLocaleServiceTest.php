e14e279f68d60c5f06a7329b22c6b8bc1b54e0c7
MathieuGTR <mathieu@gtrsuite.com>
2023-10-09 14:46:38 +0200

    chore(tests): use getMockBuilder

--- tests/ServicesTests/GeonamesCountryLocaleServiceTest.php
+++ tests/ServicesTests/GeonamesCountryLocaleServiceTest.php
@@ -2,30 +2,57 @@
 
 namespace App\tests\ServicesTests;
 
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\MockObject\MockObject;
 use App\Service\GeonamesCountryLocaleService;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class GeonamesCountryLocaleServiceTest extends KernelTestCase
+class GeonamesCountryLocaleServiceTest extends WebTestCase
 {
+    protected MockObject $serviceMock;
+
+    #[TestDox('Mock setup')]
+    public function setUp(): void
+    {
+        $this->serviceMock = $this->getMockBuilder(GeonamesCountryLocaleService::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['updateCountryBatch', 'updateCountrySingle', 'getCountryNamesForLocale'])
+            ->getMock();
+
+        self::getContainer()->set(GeonamesCountryLocaleService::class, $this->serviceMock);
+    }
+
     public function testShouldUpdateCountryBatch(): void
     {
-        self::bootKernel();
-        $container = static::getContainer();
-        $service = $container->get(GeonamesCountryLocaleService::class);
-        $update = $service->updateCountryBatch(1);
+        $this->serviceMock->method('updateCountryBatch')->willReturn('"all elements in this file have already been imported"');
 
-        $this->assertNotEquals('Random-string', $update, 'Hummmm sorry sweaty...');
-        $this->assertSame('"all elements in this file have already been imported"', $update);
+        $this->serviceMock->expects($this->once())->method('updateCountryBatch');
+        $this->serviceMock->updateCountryBatch(1);
     }
 
     public function testShouldUpdateCountrySingle(): void
     {
-        self::bootKernel();
-        $container = static::getContainer();
-        $service = $container->get(GeonamesCountryLocaleService::class);
-        $update = $service->updateCountrySingle(895949);
+        $this->serviceMock->expects($this->any())
+            ->method('updateCountrySingle')
+            ->willReturn('The ID 895949 has already been imported');
 
-        $this->assertNotEquals('Random-string', $update, 'Hummmm sorry sweaty...');
-        $this->assertSame('The ID 895949 has already been imported', $update, 'Hummmm sorry sweaty...');
+        $this->assertEquals(
+            null,
+            $this->serviceMock->updateCountrySingle,
+            'OH NO !'
+        );
+
+        $this->serviceMock->expects($this->once())->method('updateCountrySingle');
+        $this->serviceMock->updateCountrySingle(895949);
+    }
+
+    public function testGetCountryNamesForLocale(): void
+    {
+        $this->serviceMock->method('getCountryNamesForLocale')->willReturn(new JsonResponse());
+        $this->assertSame(null, $this->serviceMock->getCountryNamesForLocale);
+
+        $this->serviceMock->expects($this->once())->method('getCountryNamesForLocale');
+        $this->serviceMock->getCountryNamesForLocale('fr');
     }
 }
