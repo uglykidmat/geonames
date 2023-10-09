2334603748d8bece3f2856612c2d7e15802c39a0
MathieuGTR <mathieu@gtrsuite.com>
2023-10-09 17:20:39 +0200

    feat(sonde): cleaner JsonResponse

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StatusController extends AbstractController
 {
     #[Route('/status', name: 'status')]
-    public function status(): Response
+    public function status(): JsonResponse
     {
-        return new Response('ok');
+        $response = new JsonResponse();
+        $response->setContent(json_encode(['status' => 'up']));
+        return $response;
     }
 }
