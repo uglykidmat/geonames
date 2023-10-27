c914b891820897ae227449fb00851ccd68223fb9
MathieuGTR <mathieu@gtrsuite.com>
2023-10-27 11:00:00 +0200

    feat(ApiPlatform): fix redundant attributes + tags update

--- src/Controller/GeojsonController.php
+++ src/Controller/GeojsonController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use OpenApi\Attributes as OA;
 
 #[Route('/geojson', name: 'geojson')]
+#[OA\Tag(name: 'Geonames GeoJson')]
 class GeojsonController extends AbstractController
 {
     public function __construct(
@@ -17,21 +18,18 @@ class GeojsonController extends AbstractController
     }
 
     #[Route('/update', name: 'geojson_update', methods: ['GET', 'HEAD'])]
-    #[OA\Tag(name: 'Geonames GeoJson')]
     public function update(): JsonResponse
     {
         return $this->service->updateAll();
     }
 
     #[Route('/get', name: 'geojson_get_all', methods: ['GET', 'HEAD'])]
-    #[OA\Tag(name: 'Geonames GeoJson')]
     public function getAll(): JsonResponse
     {
         return $this->service->getAll();
     }
 
     #[Route('/get/{id}', name: 'geojson_get_one', methods: ['GET', 'HEAD'])]
-    #[OA\Tag(name: 'Geonames GeoJson')]
     public function getOne($id): JsonResponse
     {
         return $this->service->getOne($id);
--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use OpenApi\Attributes as OA;
 
 class StatusController extends AbstractController
 {
     #[Route('/status', name: 'status')]
+    #[OA\Tag(name: 'Geonames Status')]
     public function status(): JsonResponse
     {
         $response = new JsonResponse();
