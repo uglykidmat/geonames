f531ad9f5531e3c27f7eaaadf6f6bea1f84311e4
MathieuGTR <mathieu@gtrsuite.com>
2023-11-20 14:04:36 +0100

    fix(locales): link, lauc, abbr, wkdt removed from fetching

--- src/Service/AdministrativeDivisionLocaleService.php
+++ src/Service/AdministrativeDivisionLocaleService.php
@@ -65,7 +65,7 @@ class AdministrativeDivisionLocaleService
         $newLocalesCount = 0;
         $apiResponse = $this->apiservice->getJsonSearch($geonameId);
         foreach ($apiResponse->alternateNames as $localeItem) {
-            if (isset($localeItem->lang) && $localeItem->lang != 'link' && $localeItem->lang != 'abbr') {
+            if (isset($localeItem->lang) &&  !in_array($localeItem->lang, ['link', 'lauc', 'abbr', 'wkdt'])) {
                 if (!$this->entityManager->getRepository(AdministrativeDivisionLocale::class)
                     ->findOneBy(array(
                         'geonameId' => $geonameId,
