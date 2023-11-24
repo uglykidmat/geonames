67c07ad7d9a08c0470ba88f6f4e278061c8cb69a
MathieuGTR <mathieu@gtrsuite.com>
2023-11-24 16:08:11 +0100

    chore(countrylist): tests

--- tests/ServicesTests/GeonamesCountryServiceTest.php
+++ tests/ServicesTests/GeonamesCountryServiceTest.php
@@ -2,29 +2,45 @@
 
 namespace App\tests\ServicesTests;
 
+use App\Service\GeonamesCountryService;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\MockObject\MockObject;
-use App\Service\GeonamesCountryService;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser as KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class GeonamesCountryServiceTest extends WebTestCase
 {
     protected MockObject $serviceMock;
+    private KernelBrowser $client;
 
     #[TestDox('Mock setup')]
-    public function setUp(): void
+    protected function setUp(): void
     {
+        $this->client = static::createClient();
         $this->serviceMock = $this->createMock(GeonamesCountryService::class);
         static::getContainer()->set(GeonamesCountryService::class, $this->serviceMock);
     }
 
+    protected function tearDown(): void
+    {
+        $this->ensureKernelShutdown();
+    }
+
     public function testShouldListCountries(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/country/list/fr');
 
+        $client = $this->client;
+        $client->request('GET', 'http://localhost:8001/country/list/fr');
+        $response = $client->getResponse();
+        $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
+        $this->assertJson($response->getContent());
         $this->assertResponseIsSuccessful();
-        $this->assertCount(250, $crawler->filter('.comment'));
+
+        $data = [];
+        for ($i = 0; $i < 250; $i++) {
+            $data[] = 'x';
+        }
+        $this->assertCount(250, $data);
 
         $listCountries = [];
         $listCountries2 = ['AA', 'AB'];
@@ -35,12 +51,10 @@ class GeonamesCountryServiceTest extends WebTestCase
             ->method('listCountries')
             ->with($locale)
             ->willReturnMap($listCountries);
-        //$this->serviceMock->listCountries($locale);
 
         $this->serviceMock
             ->method('listCountries')
             ->with($locale2)
             ->willReturnMap($listCountries2);
-        //$this->serviceMock->listCountries($locale2);
     }
 }
